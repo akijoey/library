3171ae021716a726bdfb096f5789dfc03926433d
AkiJoey <akijoey1010635951@gmail.com>
2020-03-26 21:04:44 +0800

    add vuex

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
   outputDir: 'src/main/resources/static',
   configureWebpack: {
     entry: './src/main/webapp/main.js'
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src/main/webapp'))
   }
 }
