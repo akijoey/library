3a3ae0025c5f7cae97134875e8846e2990f4aca8
AkiJoey <akijoey1010635951@gmail.com>
2020-04-28 21:15:20 +0800

    style: view login

--- src/main/webapp/views/login/index.vue
+++ src/main/webapp/views/login/index.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="login-container">
-    <el-form :model="form" :rules="rules" ref="form" class="login-form" label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <icon-font icon-class="user" />
-        </span>
+  <div id="login-container">
+    <el-form :model="form" :rules="rules" ref="form" label-position="left">
+      <el-form-item id="form-0">
+        <h1>Login Form</h1>
+      </el-form-item>
+      <el-form-item prop="username" id="form-1">
+        <icon-font icon-class="user" />
         <el-input type="text" v-model="form.username" placeholder="Username" ref="username" />
       </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <icon-font icon-class="password" />
-        </span>
+      <el-form-item prop="password" id="form-2">
+        <icon-font icon-class="password" />
         <el-input :type="password" v-model="form.password" placeholder="Password" ref="password" @keyup.enter.native="handleLogin" />
-        <span class="show-pwd" @click="showPassword">
-          <icon-font :icon-class="eye" />
-        </span>
+        <icon-font :icon-class="eye" @click.native="showPassword" />
       </el-form-item>
-
       <el-form-item id="form-3">
         <el-button :loading="loading" type="primary" @click="handleLogin">Login</el-button>
         <el-button type="primary" @click="handleRegister">Register</el-button>
@@ -120,6 +110,7 @@ export default {
     handleRegister() {
       this.$refs.form.validate(valid => {
         if (valid) {
+          // register api
           console.log('submit')
         } else {
           console.log('Error Submit')
@@ -132,108 +123,72 @@ export default {
 </script>
 
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
+  // fix: input cursor color
+  $cursor: #fff;
+  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+    #login-container .el-input input {
+      color: $cursor;
+    }
   }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
+  #login-container {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient(
+      125deg, #2c3e50, #16a085
+    );
+    .el-form {
+      width: 450px;
+      @for $i from 0 to 4 {
+        #form-#{$i} {
+          animation: up (.5s + $i * .1);
+          margin-bottom: 25px;
+          &:nth-child(2), &:nth-child(3) {
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            background: rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            color: #454545;
+          }
+          h1 {
+            color: #eee;
+            font-size: 26px;
+            font-weight: bold;
+            text-align: center;
+            margin-top: 0;
+          }
+          .el-input {
+            width: 78%;
+            input {
+              border: 0;
+              color: #fff;
+              background: transparent;
+              -webkit-appearance: none;
+              caret-color: $cursor;
+              &:-webkit-autofill {
+                box-shadow: 0 0 0px 1000px #283443 inset !important;
+                -webkit-text-fill-color: $cursor !important;
+              }
+            }
+          }
+          svg {
+            color: #889aa4;
+            margin: 17px 5px 17px 15px;
+            vertical-align: middle;
+            user-select: none;
+            &:nth-child(3) {
+              cursor: pointer;
+            }
+          }
+          button {
+            width: 47.5%;
+            margin-bottom: 50px;
+            & + button {
+              margin-left: 5% !important;
+            }
+          }
+        }
       }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-
-.login-container {
-  min-height: 100vh;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-}
-
-  button {
-    width: 48%;
-    margin-bottom: 30px;
-    & + button {
-      margin-left: 4% !important;
     }
   }
 </style>
