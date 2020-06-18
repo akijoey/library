314e2521d7c96a3ab7294c7484a2c3258cba1c93
AkiJoey <akijoey1010635951@gmail.com>
2020-06-19 07:32:21 +0800

    style: home responsive

--- src/main/webapp/views/home/index.vue
+++ src/main/webapp/views/home/index.vue
@@ -36,35 +36,47 @@
 </script>
 
 <style lang="scss" scoped>
-  main {
-    display: flex;
-    align-items: center;
-    height: calc(100vh - 10rem - 40px);
-    img {
-      width: 350px;
-      animation: up .7s;
+  .el-container {
+    flex-wrap: wrap;
+    main {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: calc(100vh - 10rem - 40px);
+      img {
+        width: 350px;
+        animation: up .7s;
+      }
+      h1 {
+        font-size: 5rem;
+        font-family: Trebuchet MS;
+        margin: 0 0 30px 10px;
+        animation: down .8s .2s backwards;
+      }
+      p {
+        font-size: 2rem;
+        font-family: Arial;
+        line-height: 2.5rem;
+        margin: 10px;
+        animation: down .8s;
+      }
+      span, svg {
+        font-size: 1rem;
+        font-weight: bold;
+        margin-left: -4px;
+      }
+      button {
+        margin: 25px 15px 0 10px;
+        animation: port .8s;
+      }
     }
-    h1 {
-      font-size: 5rem;
-      font-family: Trebuchet MS;
-      margin: 0 0 30px 10px;
-      animation: down .8s .2s backwards;
-    }
-    p {
-      font-size: 2rem;
-      font-family: Arial;
-      line-height: 2.5rem;
-      margin: 10px;
-      animation: down .8s;
-    }
-    span, svg {
-      font-size: 1rem;
-      font-weight: bold;
-      margin-left: -4px;
-    }
-    button {
-      margin: 25px 15px 0 10px;
-      animation: port .8s;
+    @media screen and (max-width: 930px) {
+      main {
+        height: auto;
+        text-align: center;
+      }
     }
   }
+  
+  
 </style>
